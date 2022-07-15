e6649f4d958f003738ff338c150ed08fa3620848
bessyhuang <bessyhuang1@gmail.com>
2022-07-15 11:28:10 +0800

    Modify options for produce selected markers VCF

--- Hardy-Weinberg_Equilibrium/4-4.Select_markers_Produce_VCF_Run_PCA.py
+++ Hardy-Weinberg_Equilibrium/4-4.Select_markers_Produce_VCF_Run_PCA.py
@@ -1,13 +1,20 @@
 from collections import defaultdict
 import os
 
+
+MarkersSet_type = input("Please Enter number of `MarkersSet_type` that you want: \n\
+e.g. \n\
+(1) ALL_markers	\t\
+(2) AFF_markers	\t\
+(3) UNAFF_markers \t\
+(4) AFF_markers (significant) & UNAFF_markers (no significant) \n\
+(5) UNAFF_markers (significant) & AFF_markers (no significant) \n\
+(6) Total markers (no significant) \n\
+(7) No filter any Marker\n\n> ")
+
 disease = 'Fabry_Aging'
 Amount_of_Samples = '186'
 P_value = '0.0001'
-
-#MidOutput_FilePath='/staging/reserve/aging/chia2831/FabryDisease/Merge_Fabry_Aging_total_186/Replaced_header_vcf/HardyWeinberg_result/MiddleOutput_Filtered/'
-#os.system("[ ! -d {} ] && mkdir -p {}".format(MidOutput_FilePath, MidOutput_FilePath))
-#MidOutput_HWE_filter_prefix="{}_{}_HWE_Filter_p-value_{}".format(disease, Amount_of_Samples, P_value)
 
 CaseControl_table = 'CaseControl_record_table_186.txt'
 
@@ -21,15 +28,15 @@ Input_HWE_filtered_UNAFF_FileName = "{}_{}_HWE_Filter_p-value_{}_UNAFF.txt".form
 
 Output_FilePath='/staging/reserve/aging/chia2831/FabryDisease/Merge_Fabry_Aging_total_186/Replaced_header_vcf/HardyWeinberg_result/Create_HWE_filtered_VCF/'
 os.system("[ ! -d {} ] && mkdir -p {}".format(Output_FilePath, Output_FilePath))
-Output_HWE_filtered_VCF_FileName = "{}_{}_HWE_Filtered_p-value_{}.vcf".format(disease, Amount_of_Samples, P_value)
-Output_HWE_filtered_bfile_prefix = "{}_{}_HWE_Filtered_{}_bfile".format(disease, Amount_of_Samples, P_value)
-Output_PCA_HWE_filtered_prefix = "{}_{}_HWE_Filtered_{}_PCA".format(disease, Amount_of_Samples, P_value)
+Output_HWE_filtered_VCF_FileName = "{}_{}_HWE_Filtered_p-value_{}_Expt{}.vcf".format(disease, Amount_of_Samples, P_value, MarkersSet_type)
+Output_HWE_filtered_bfile_prefix = "{}_{}_HWE_Filtered_{}_bfile_Expt{}".format(disease, Amount_of_Samples, P_value, MarkersSet_type)
+Output_PCA_HWE_filtered_prefix = "{}_{}_HWE_Filtered_{}_PCA_Expt{}".format(disease, Amount_of_Samples, P_value, MarkersSet_type)
 
-def HWE_filtered_variants_dict(CaseControl_type, line_list):
+def HWE_filtered_variants_dict(CaseControl_type, content_list):
 	significant_diff_markers_dict = defaultdict(list)
 	row_list = list()
 	if CaseControl_type == 'ALL_CaseControl':
-		for index, line in enumerate(line_list):
+		for index, line in enumerate(content_list):
 			if index != 0:
 				row_list = line.strip().split('\t')
 				chrN = row_list[0]
@@ -37,66 +44,118 @@ def HWE_filtered_variants_dict(CaseControl_type, line_list):
 				significant_diff_markers_dict[chrN].append(pos)
 			else:
 				pass
+		return significant_diff_markers_dict
 	elif CaseControl_type == 'AFF_Case':
-		for index, line in enumerate(line_list):
+		for index, line in enumerate(content_list):
 			if index != 0:
-				row_line = line.strip().split('\t')
+				row_list = line.strip().split('\t')
 				chrN = row_list[0]
 				pos = row_list[1]
 				significant_diff_markers_dict[chrN].append(pos)
 			else:
 				pass
+		return significant_diff_markers_dict
 	elif CaseControl_type == 'UNAFF_Control':
-		for index, line in enumerate(line_list):
+		for index, line in enumerate(content_list):
 			if index != 0:
-				row_line = line.strip().split('\t')
+				row_list = line.strip().split('\t')
 				chrN = row_list[0]
 				pos = row_list[1]
 				significant_diff_markers_dict[chrN].append(pos)
 			else:
 				pass
+		return significant_diff_markers_dict
 	else:
-		pass
-	return significant_diff_markers_dict
+		print('--------', row_list)
 
+def Final_selected_markers_dict(MarkersSet_type, HWE_ALL_dict, HWE_AFF_dict, HWE_UNAFF_dict):
+	selected_markers_dict = defaultdict(list)
+	if MarkersSet_type == 'ALL_markers' or MarkersSet_type == '1':
+		return HWE_ALL_dict
+	elif MarkersSet_type == 'AFF_markers' or MarkersSet_type == '2':
+		return HWE_AFF_dict
+	elif MarkersSet_type == 'UNAFF_markers' or MarkersSet_type == '3':
+		return HWE_UNAFF_dict
+	elif MarkersSet_type == 'AFF_markers (significant) & UNAFF_markers (no significant)' or MarkersSet_type == '4':
+		for chrN in HWE_AFF_dict:
+			List1 = HWE_AFF_dict[chrN]
+			List2 = HWE_UNAFF_dict[chrN]
+			# 判斷：List2 內的 data 是否都有在 List1 內出現
+			#check = all(item in List1 for item in List2)
+
+			# List1 sig. - List2 sig. => AFF (sig.) and UNAFF (no sig.)
+			new_List = list(set(List1) - set(List2))
+			selected_markers_dict[chrN] = new_List
+		return selected_markers_dict
+	elif MarkersSet_type ==	'UNAFF_markers (significant) & AFF_markers (no significant)' or MarkersSet_type == '5':
+		for chrN in HWE_UNAFF_dict:
+			List1 = HWE_UNAFF_dict[chrN]
+			List2 = HWE_AFF_dict[chrN]
+			
+			# List1	sig. - List2 sig. => UNAFF (sig.) and AFF (no sig.)
+			new_List = list(set(List1) - set(List2))
+			selected_markers_dict[chrN] = new_List
+		return selected_markers_dict
+	elif MarkersSet_type == 'Total markers (no significant)' or MarkersSet_type == '6':
+		for chrN in HWE_UNAFF_dict:
+			List1 = HWE_ALL_dict[chrN]
+			List2 = HWE_AFF_dict[chrN]
+			List3 = HWE_UNAFF_dict[chrN]
+
+			# List1 sig. + List2 sig. + List2 sig. => ALL (sig.) or AFF (sig.) or UNAFF (sig.)
+			new_List = list(set(List1) | set(List2) | set(List3))
+			selected_markers_dict[chrN] = new_List
+		return selected_markers_dict		
+	elif MarkersSet_type == 'No filter any Marker' or MarkersSet_type == '7':
+		return None
 
 # Read selected markers file (HWE p-value < 0.0001)
 with open(Input_HWE_FilePath + Input_HWE_filtered_ALL_FileName, 'r') as f_in:
-	line_list = f_in.readlines()
-	ALL_CaseControl_HWE_dict = HWE_filtered_variants_dict('ALL_CaseControl', line_list)		
+	content_list = f_in.readlines()
+	ALL_CaseControl_HWE_dict = HWE_filtered_variants_dict('ALL_CaseControl', content_list)		
 
-"""
 with open(Input_HWE_FilePath + Input_HWE_filtered_AFF_FileName, 'r') as f_in:
-        line_list = f_in.readlines()
-        AFF_Case_HWE_dict = HWE_filtered_variants_dict('AFF_Case', line_list)
+	content_list = f_in.readlines()
+	AFF_Case_HWE_dict = HWE_filtered_variants_dict('AFF_Case', content_list)
 
 with open(Input_HWE_FilePath + Input_HWE_filtered_UNAFF_FileName, 'r') as f_in:
-        line_list = f_in.readlines()
-        UNAFF_Control_HWE_dict = HWE_filtered_variants_dict('UNAFF_Control', line_list)
-"""
+	content_list = f_in.readlines()
+	UNAFF_Control_HWE_dict = HWE_filtered_variants_dict('UNAFF_Control', content_list)
+
 
 # Create VCF file (selected markers)
 f_out = open(Output_FilePath + Output_HWE_filtered_VCF_FileName, 'w')
+
+Selected_Markers_dict = Final_selected_markers_dict(MarkersSet_type, ALL_CaseControl_HWE_dict, AFF_Case_HWE_dict, UNAFF_Control_HWE_dict)
 
 with open(Input_VCF_FilePath + Input_VCF_FileName, 'r') as f:
 	line = f.readline()
 	while line != '':
 		if line.startswith('#'):
 			f_out.write(line)
+		elif MarkersSet_type == 'Total markers (no significant)' or MarkersSet_type == '6':
+			line_list = line.split('\t')
+			VCF_chrN = line_list[0]
+			VCF_pos = line_list[1]
+			
+			# Total markers - [ ALL (sig.) markers or Delete AFF (sig.) markers or Delete UNAFF (sig.) markers ]
+			if VCF_pos in Selected_Markers_dict[VCF_chrN]:
+				pass
+			else:
+				f_out.write(line)
+		elif MarkersSet_type == 'No filter any Marker' or MarkersSet_type == '7':
+			# Equal to `RAW VCF file` (no filter, input all VCF markers then do PCA)
+			f_out.write(line)
 		else:
-			try:
-				line_list = line.split('\t')
-				VCF_chrN = line_list[0]
-				VCF_pos = line_list[1]
+			line_list = line.split('\t')
+			VCF_chrN = line_list[0]
+			VCF_pos = line_list[1]
 
-				if VCF_pos in ALL_CaseControl_HWE_dict[VCF_chrN]:
-					f_out.write(line)
-				else:
-					pass 
-			except:
-				print('Can not split =>', line)
+			if VCF_pos in Selected_Markers_dict[VCF_chrN]:
+				f_out.write(line)
+			else:
+				pass 
 		line = f.readline()
-
 f_out.close()
 
 
@@ -135,5 +194,5 @@ f_out.close()
 
 os.system("/staging/reserve/aging/chia2831/bin/plink --bfile {} --pca --out {}".format(Output_FilePath + Output_HWE_filtered_bfile_prefix, Output_FilePath + Output_PCA_HWE_filtered_prefix))
 
-# Delete MidOutput folder
-#os.system("rm -r {}".format(MidOutput_FilePath))
+# Delete MidOutput
+os.system("rm -r {}".format(Output_FilePath + 'Modify_SexCode_' + Output_HWE_filtered_bfile_prefix + '.fam'))
