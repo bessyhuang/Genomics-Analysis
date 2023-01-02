ce4417ee3d2934b09fdb59d71378c84cdc998ac8
bessyhuang <bessyhuang1@gmail.com>
2023-01-02 22:57:09 +0800

    Update: Filter_each_vcf

--- Bio-preprocessing/Filter_each_vcf/1.FixBatch1_Filter_each_vcf.sh
+++ Bio-preprocessing/Filter_each_vcf/1.FixBatch1_Filter_each_vcf.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
-filename='Aging_Male.list'
-current_dir='/staging/reserve/aging/chia2831/FabryDisease/Aging_HALST_Male_cohort/'
-
+filename='2000_genome.samplelist'
+samplelist_dir='/staging2/reserve/flagship/chia2831/TEST_2000_genome_VCFgz/Genomics-Analysis/Bio-preprocessing/Filter_each_vcf/'
+VCF_dir='/staging2/reserve/flagship/chia2831/TEST_2000_genome_VCFgz/VCF/'
 
 ## Check for dir, if not found create it using the mkdir ##
-create_dir="/staging/reserve/aging/chia2831/FabryDisease/Aging_HALST_Male_cohort/recode_vcf/"
+create_dir='/staging2/reserve/flagship/chia2831/TEST_2000_genome_VCFgz/recode_vcf/'
 [ ! -d "$create_dir" ] && mkdir -p "$create_dir"
 output_path=${create_dir}
-
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
 	# Check file exist or not
         echo "==> $line"
-        echo -e "`ls ${current_dir} | grep ${line}`"
+        echo -e "`ls ${VCF_dir} | grep ${line}`"
 
-
+	
 	# Modify : --vcf
-	/staging/reserve/aging/chia2831/bin/vcftools --vcf "${current_dir}${line}".vcf --recode --recode-INFO-all \
+	/staging/reserve/aging/chia2831/bin/vcftools --vcf "${VCF_dir}${line}".vcf --recode --recode-INFO-all \
 		--out ${output_path}${line}_DP10_MAF21.vcf \
 		--chr chr1 --chr chr2 --chr chr3 --chr chr4 --chr chr5 --chr chr6 --chr chr7 --chr chr8 --chr chr9 --chr chr10 --chr chr11 --chr chr12 --chr chr13 --chr chr14 --chr chr15 --chr chr16 --chr chr17 --chr chr18 --chr chr19 --chr chr20 --chr chr21 --chr chr22 --chr chrX --chr chrY --chr chrM \
 		--min-meanDP 10 \
 		--non-ref-af-any 0.21
-done < $filename
+done < ${samplelist_dir}${filename}
--- Bio-preprocessing/Filter_each_vcf/1.FixBatch1_Filter_each_vcfgz.sh
+++ Bio-preprocessing/Filter_each_vcf/1.FixBatch1_Filter_each_vcfgz.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
-filename='CaseControl_1-2093.list'
-current_dir='/staging/reserve/aging/chia2831/2000_genome_VCFgz/'
+filename='2000_genome.samplelist'
+samplelist_dir='/staging2/reserve/flagship/chia2831/TEST_2000_genome_VCFgz/Genomics-Analysis/Bio-preprocessing/Filter_each_vcf/'
+VCFgz_dir='/staging2/reserve/flagship/chia2831/TEST_2000_genome_VCFgz/VCFgz/'
 
 ## Check for dir, if not found create it using the mkdir ##
-create_dir="/staging/reserve/aging/chia2831/2000_genome_VCFgz/recode_vcf/"
+create_dir='/staging2/reserve/flagship/chia2831/TEST_2000_genome_VCFgz/recode_vcf/'
 [ ! -d "$create_dir" ] && mkdir -p "$create_dir"
 output_path=${create_dir}
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
 	# Check file exist or not
         echo "==> $line"
-        echo -e "`ls ${current_dir} | grep ${line}`"
+        echo -e "`ls ${VCFgz_dir} | grep ${line}`"
 
 	
 	# Modify : --gzvcf
-	/staging/reserve/aging/chia2831/bin/vcftools --gzvcf "${current_dir}${line}".vcf.gz --recode --recode-INFO-all \
+	/staging/reserve/aging/chia2831/bin/vcftools --gzvcf "${VCFgz_dir}${line}".vcf.gz --recode --recode-INFO-all \
 		--out ${output_path}${line}_DP10_MAF21.vcf \
 		--chr chr1 --chr chr2 --chr chr3 --chr chr4 --chr chr5 --chr chr6 --chr chr7 --chr chr8 --chr chr9 --chr chr10 --chr chr11 --chr chr12 --chr chr13 --chr chr14 --chr chr15 --chr chr16 --chr chr17 --chr chr18 --chr chr19 --chr chr20 --chr chr21 --chr chr22 --chr chrX --chr chrY --chr chrM \
 		--min-meanDP 10 \
 		--non-ref-af-any 0.21
-done < $filename
+done < ${samplelist_dir}${filename}
