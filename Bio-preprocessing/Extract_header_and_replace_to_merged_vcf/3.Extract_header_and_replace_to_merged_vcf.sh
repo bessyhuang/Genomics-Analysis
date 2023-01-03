292ed668257bae7f2c35ca0e77111fe708ecb901
bessyhuang <bessyhuang1@gmail.com>
2023-01-03 14:59:43 +0800

    Update: Extract_header_and_replace_to_merged_vcf

--- Bio-preprocessing/Extract_header_and_replace_to_merged_vcf/3.Extract_header_and_replace_to_merged_vcf.sh
+++ Bio-preprocessing/Extract_header_and_replace_to_merged_vcf/3.Extract_header_and_replace_to_merged_vcf.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 
-current_dir='/staging/reserve/aging/chia2831/FabryDisease/Fabry_Data_93/Merge_Fabry_total_93/'
-disease='FabryDisease'
-Amount_of_Samples='93'
+module load biology/bcftools/1.13
 
+merged_vcf_dir='/staging2/reserve/flagship/chia2831/TEST_2000_genome_VCFgz/Merge_2000_genome/'
+disease='Genome'
+Amount_of_Samples='2000'
 
 
 # Extract the VCF header from joint VCF file
-awk '{if(/^#/)print;else exit}' ${current_dir}${disease}_${Amount_of_Samples}.merge.vcf > ${current_dir}header_VCF.txt
+awk '{if(/^#/)print;else exit}' ${merged_vcf_dir}${disease}_${Amount_of_Samples}.merge.vcf > ${merged_vcf_dir}header_VCF.txt
 
 
 # Modify header
-sed '1,49!d' ${current_dir}header_VCF.txt > ${current_dir}NEW_UP_header_VCF.txt
+sed '1,49!d' ${merged_vcf_dir}header_VCF.txt > ${merged_vcf_dir}NEW_UP_header_VCF.txt
 
 ## (1)【 While use bcftools merge 】
-sed '3391,3393!d' ${current_dir}header_VCF.txt > ${current_dir}NEW_DOWN_header_VCF.txt
+sed '3391,3393!d' ${merged_vcf_dir}header_VCF.txt > ${merged_vcf_dir}NEW_DOWN_header_VCF.txt
 ## (2)【 While use bcftools concat 】
-#sed '3391,3397!d' ${current_dir}header_VCF.txt > ${current_dir}NEW_DOWN_header_VCF.txt
+#sed '3391,3397!d' ${merged_vcf_dir}header_VCF.txt > ${merged_vcf_dir}NEW_DOWN_header_VCF.txt
 
-cat ${current_dir}NEW_UP_header_VCF.txt ${current_dir}NEW_DOWN_header_VCF.txt > ${current_dir}NEW_header_VCF.txt
+cat ${merged_vcf_dir}NEW_UP_header_VCF.txt ${merged_vcf_dir}NEW_DOWN_header_VCF.txt > ${merged_vcf_dir}NEW_header_VCF.txt
 
 
 # Replace header
-create_dir='/staging/reserve/aging/chia2831/FabryDisease/Fabry_Data_93/Merge_Fabry_total_93/Replaced_header_vcf/'
+create_dir='/staging2/reserve/flagship/chia2831/TEST_2000_genome_VCFgz/Merge_2000_genome/Replaced_header_vcf/'
 [ ! -d "$create_dir" ] && mkdir -p "$create_dir"
 output_path=${create_dir}
 
-/staging/reserve/aging/chia2831/bin/bcftools reheader -h ${current_dir}NEW_header_VCF.txt -o ${output_path}new_${disease}_${Amount_of_Samples}.merge.vcf ${current_dir}${disease}_${Amount_of_Samples}.merge.vcf 
+bcftools reheader -h ${merged_vcf_dir}NEW_header_VCF.txt -o ${output_path}new_${disease}_${Amount_of_Samples}.merge.vcf ${merged_vcf_dir}${disease}_${Amount_of_Samples}.merge.vcf 
