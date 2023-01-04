4951793067b83cc58bac184cffff6c5a124870ae
bessyhuang <bessyhuang1@gmail.com>
2023-01-04 11:50:18 +0800

    Update: Extract_specific_gene_position.py

--- Advanced_Analysis/Extract_specific_gene_position_on_merged_vcf/Extract_specific_gene_position.py
+++ Advanced_Analysis/Extract_specific_gene_position_on_merged_vcf/Extract_specific_gene_position.py
@@ -1,9 +1,9 @@
 import os
 
-FilePath = '/staging/reserve/aging/chia2831/FabryDisease/Merge_Fabry_Aging_total_186/HardyWeinberg_result/Extract_Specific_Gene_Position/'
-Filename = 'chrX_Fabry.vcf'
-Amount_of_Samples = '186'
-
+FilePath = '/staging2/reserve/flagship/chia2831/TEST_2000_genome_VCFgz/Merge_2000_genome_ByChr/merged_recode_vcf/All_chr/FinalResult_here/Replaced_header_vcf/'
+Filename = 'new_Genome_2000.merge.vcf'
+Disease = 'Genome'
+Amount_of_Samples = '2000'
 
 
 # 擷取特定 chromosome
@@ -11,26 +11,20 @@ GeneName = input('Please Enter "GeneName" (e.g. GLA_IVS4)\n > ')
 chr_N = input('Please Enter "chromosome" (e.g. chrX)\n > ')
 pos = input('Please Enter "position" (e.g. 101399747)\n > ')
 
-
-Output_FilePath = '/staging/reserve/aging/chia2831/FabryDisease/Merge_Fabry_Aging_total_186/HardyWeinberg_result/Extract_Specific_Gene_Position/chrX_20K/'
+Output_FilePath = '/staging2/reserve/flagship/chia2831/TEST_2000_genome_VCFgz/Merge_2000_genome_ByChr/merged_recode_vcf/All_chr/FinalResult_here/Replaced_header_vcf/chrX_10K_up_down/'
 os.system('[ ! -d {} ] && mkdir -p {}'.format(Output_FilePath, Output_FilePath))
 
-Output_merge_vcfgz = 'new_FabryDisease_{}.merge.vcf.gz'.format(Amount_of_Samples)
-Output_chrN_vcf = '{}_Fabry.vcf'.format(chr_N)
-Output_gene_pos_vcf = 'new_FabryDisease_{}_{}_gene.merge.vcf'.format(Amount_of_Samples, GeneName)
+Output_merge_vcfgz = 'new_{}_{}.merge.vcf.gz'.format(Disease, Amount_of_Samples)
+Output_chrN_vcf = '{}_{}.vcf'.format(chr_N, Disease)
+Output_gene_pos_vcf = 'new_{}_{}_{}_gene.merge.vcf'.format(Disease, Amount_of_Samples, GeneName)
 
-
-# 補充：Extract header
-# Method 1
-#cat new_FabryDisease_70.merge.vcf | grep '^#' > test_header.txt
-# Method 2 `NEW_header_VCF.txt`
 
 
 # 1. 製作 vcf.gz 和 vcf.gz.tbi
-os.system("/staging/reserve/aging/chia2831/bin/bgzip -c {} > {}".format(FilePath + Filename, Output_FilePath + Output_merge_vcfgz))
-os.system("/staging/reserve/aging/chia2831/bin/tabix -p vcf {}".format(Output_FilePath + Output_merge_vcfgz))
+os.system("module load biology/Samtools/1.15.1")
+os.system("bgzip -c {} > {}".format(FilePath + Filename, Output_FilePath + Output_merge_vcfgz))
+os.system("tabix -p vcf {}".format(Output_FilePath + Output_merge_vcfgz))
 print("製作 vcf.gz 和 vcf.gz.tbi ==> Finish!")
-
 
 # 2. 擷取特定 chromosome position
 pos_minus_10K = int(pos) - 10000
@@ -38,7 +32,8 @@ pos_plus_10K = int(pos) + 10000
 print("The range of chromosome positions to be extracted \t=> {} ~ {}".format(pos_minus_10K, pos_plus_10K))
 
 
-os.system("/staging/reserve/aging/chia2831/bin/bcftools view {} --regions {} -o {} -Ov".format(Output_FilePath + Output_merge_vcfgz, chr_N, Output_FilePath + Output_chrN_vcf))
+os.system("module load biology/bcftools/1.13")
+os.system("bcftools view {} --regions {} -o {} -Ov".format(Output_FilePath + Output_merge_vcfgz, chr_N, Output_FilePath + Output_chrN_vcf))
 
 
 # Read lines in `chrX_Fabry.vcf`
