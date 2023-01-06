cd9a7d981eb991adac079193886803259e2a8b1a
bessyhuang <bessyhuang1@gmail.com>
2023-01-06 13:32:16 +0800

    Update: Grep_Sequence_QC_result

--- Basic_Statistic/Grep_Sequence_QC_result/2.FixBatch1_Get_Reads_info.sh
+++ Basic_Statistic/Grep_Sequence_QC_result/2.FixBatch1_Get_Reads_info.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-filename='CaseControl_1-70.list'
-DirName='Statistic_Data'
-current_dir='/staging/reserve/aging/chia2831/FabryDisease/Merge_Fabry_total_70/Statistic_Data/'
+filename='2000_genome.samplelist'
+filename_path='/staging2/reserve/flagship/chia2831/TEST_2000_genome_VCFgz/Basic_Statistic/Grep_Sequence_QC_result/'
+stats_dir='/staging2/reserve/flagship/chia2831/TEST_2000_genome_VCFgz/Statistic_Data/'
 
 ## Check for dir, if not found create it using the mkdir ##
-create_dir="/staging/reserve/aging/chia2831/FabryDisease/Merge_Fabry_total_70/Reads_info/"
+create_dir="/staging2/reserve/flagship/chia2831/TEST_2000_genome_VCFgz/Reads_info/"
 [ ! -d "$create_dir" ] && mkdir -p "$create_dir"
-
-vcfgz_path='/staging/reserve/aging/chia2831/FabryDisease/Output/hg38/VCF/'
-recode_vcf_path='/staging/reserve/aging/chia2831/FabryDisease/Merge_Fabry_total_70/recode_vcf/'
 output_path=${create_dir}
+
+vcfgz_path='/staging2/reserve/flagship/chia2831/TEST_2000_genome_VCFgz/VCFgz/'
+recode_vcf_path='/staging2/reserve/flagship/chia2831/TEST_2000_genome_VCFgz/recode_vcf/'
 
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
@@ -18,11 +18,11 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
 
 	# stats
 	## grep 'total length' ==> Total Yield
-	Total_Yield=`awk 'NR==24' ${current_dir}${line}.stats`
+	Total_Yield=`awk 'NR==24' ${stats_dir}${line}.stats`
 	echo -e "$line\t$Total_Yield" >> ${output_path}2_Total_Yield.txt
 
 	## CoverageDist
-	grep '^COV' ${current_dir}${line}.stats | cut -f 2- > ${output_path}grep_${line}_stats_COV_all.txt
+	grep '^COV' ${stats_dir}${line}.stats | cut -f 2- > ${output_path}grep_${line}_stats_COV_all.txt
 	### Reads <= 9 Depth
 	head -9 ${output_path}grep_${line}_stats_COV_all.txt | awk '$4=s+=$3' | grep '\[9-9\]' | awk '{print $4}' > ${output_path}${line}_CoverageDist_sum_of_First9_row.txt
 	### Reads >  9 Depth
@@ -45,17 +45,17 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
 
 	# flagstat
 	## grep 'in total'
-	RawData_Reads_1=`awk 'NR==1' ${current_dir}${line}.flagstat`
-	RawData_Reads_2=`awk 'NR==2' ${current_dir}${line}.flagstat`
+	RawData_Reads_1=`awk 'NR==1' ${stats_dir}${line}.flagstat`
+	RawData_Reads_2=`awk 'NR==2' ${stats_dir}${line}.flagstat`
 	echo -e "$line\t$RawData_Reads_1" >> ${output_path}1_RawData_Reads_QC-passed_QC-failed.txt
 	echo -e "$line\t$RawData_Reads_2\t(QC-passed reads)" >> ${output_path}1_RawData_Reads_QC-passed.txt
 
 	## grep 'duplicates'
-	Duplicates_Reads_PassQC=`awk 'NR==5' ${current_dir}${line}.flagstat`
+	Duplicates_Reads_PassQC=`awk 'NR==5' ${stats_dir}${line}.flagstat`
 	echo -e "$line\t$Duplicates_Reads_PassQC" >> ${output_path}2_Duplicates_Reads_PassQC.txt
 
 	## grep 'mapped'
-	Mappable_Reads_PassQC=`awk 'NR==7' ${current_dir}${line}.flagstat`
+	Mappable_Reads_PassQC=`awk 'NR==7' ${stats_dir}${line}.flagstat`
 	echo -e "$line\t$Mappable_Reads_PassQC" >> ${output_path}2_Mappable_Reads_PassQC.txt
 
 
@@ -64,8 +64,8 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
 
 
 	# depth
-	AvgDepth=`awk 'NR==1' ${current_dir}${line}.avg_depth`
-	Stdev=`awk 'NR==2' ${current_dir}${line}.avg_depth`
+	AvgDepth=`awk 'NR==1' ${stats_dir}${line}.avg_depth`
+	Stdev=`awk 'NR==2' ${stats_dir}${line}.avg_depth`
 	echo -e "$line\t$AvgDepth\t$Stdev" >> ${output_path}3_AvgDepth_Stdev.txt
 
 
@@ -83,4 +83,4 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
         #echo -e "$FilterVariants_with_header = $FilterVariants + 3390"
         echo -e "$line\tFilterVariants\t$FilterVariants" >> ${output_path}6_FilterVariants_yes_filter.txt	
 
-done < $filename
+done < ${filename_path}${filename}
